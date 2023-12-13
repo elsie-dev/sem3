2f9efa50dea9eff5c9c95da43a0e1ea8cb20201f
ElsieMarion <elcymao@gmail.com>
2023-12-13 11:57:51 +0300

    checking terraform workspace

--- 00_backend.tf
+++ 00_backend.tf
@@ -1,3 +1,4 @@
+#Region1
 terraform {
   backend "s3" {
     bucket                  = "elcy-sample-tfstate"
@@ -5,6 +6,20 @@ terraform {
     dynamodb_table          = "test"
     workspace_key_prefix    = "workspaces"
     region                  = "eu-west-1"
+    encrypt                 = true
+    shared_credentials_file = "~/.aws/credentials"
+    profile                 = "elcy"
+  }
+}
+
+#Region2
+terraform {
+  backend "s3" {
+    bucket                  = "elcy-sample-tfstate"
+    key                     = "sample_elcy.tfstate"
+    dynamodb_table          = "test"
+    workspace_key_prefix    = "workspaces"
+    region                  = "eu-central-1"
     encrypt                 = true
     shared_credentials_file = "~/.aws/credentials"
     profile                 = "elcy"
